0d261b6c4ca994ac0cb8aa2a676527766929a026
balkishan <shashankvishwakarma23@gmail.com>
2026-07-27 20:00:43 +0530

    Update TransactionController.java

--- FinCoreBank/finCoreBankApplication/src/main/java/edu/infosys/finCoreBankApplication/controller/TransactionController.java
+++ FinCoreBank/finCoreBankApplication/src/main/java/edu/infosys/finCoreBankApplication/controller/TransactionController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.infosys.finCoreBankApplication.bean.Transaction;
+import edu.infosys.finCoreBankApplication.dao.AccountDao;
 import edu.infosys.finCoreBankApplication.dao.TransactionDao;
 import edu.infosys.finCoreBankApplication.service.TransactionService;
 
@@ -24,45 +25,60 @@ public class TransactionController {
 	@Autowired
 	private TransactionDao transactionDao;
 	@Autowired
+	private AccountDao accountDao;
+	
+	@Autowired
 	private TransactionService service;
 	
-
-	@PostMapping("/trans")
-	public void addTransaction (@RequestBody Transaction transaction) {
-	service.balanceUpdate (transaction);
-	transactionDao.addTransaction (transaction);
-	}
-	
 	@GetMapping("/trans-info")
-	public Long getMaxTransactionId() {
-		return transactionDao.getMaxTransactionId();
+	public String generateTransactionNumber() {
+		return service.generateTransactionNumber();
 	}
-	
-	@GetMapping("/trans")
-	public List<Transaction> getAllTransactions() {
-	return transactionDao.getAllTransactions();
-	}
-	
-	@GetMapping("/trans/{id}")
-	public Transaction getTransactionById(@PathVariable String id) {
-	return transactionDao.getTransactionById(id);
-	}
-	@DeleteMapping("/trans/{id}")
-	public void deleteTranscationById(@PathVariable String id) {
-	transactionDao.deleteTranscationById(id);
-	}
+ 
 	@GetMapping("/trans-info/{cno}")
-	public List<Transaction> getTransactionByCustomer (@PathVariable Long cno) {
-	return transactionDao.getTransactionByCustomer (cno);
+	public List<Transaction> getTransactionByCustomer(@PathVariable Long cno) {
+		return transactionDao.getTransactionByCustomer(cno);
 	}
 	@GetMapping("/trans-ac/{acno}")
-	public List<Transaction> getTransactionByAccount (@PathVariable Long acno) {
-	return transactionDao.getTransactionByAccount(acno);
+	public List<Transaction> getTransactionByAccount(@PathVariable Long acno) {
+		return transactionDao.getTransactionByAccount(acno);
 	}
+ 
 	@GetMapping("/trans-type/{type}")
-	public List<Transaction> getTransactionByType (@PathVariable String type) {
-	return transactionDao.getTransactionByType (type);
+	public List<Transaction> getTransactionByType(@PathVariable String type) {
+		return transactionDao.getTransactionByType(type);
 	}
 	
+	@PostMapping("/trans")
+	public Integer addTransaction(@RequestBody Transaction transaction) {
+		int flag=0;
+	 if((transaction.getTransactionType().equalsIgnoreCase("Withdraw")) && ((accountDao.getBalanceByAccountNumber(transaction.getAccountNumber())<=5000.00))) {
+		  flag=1;
+	  }
+	  else {
+	  service.balanceUpdate(transaction);
+	  transactionDao.addTransaction(transaction);
+	  flag=2;
+	  }
+	  return flag;
+	}
+	@GetMapping("/trans")
+	public List<Transaction> getAllTransactions() {
+		return transactionDao.getAllTransactions();
+	}
+ 
+	@GetMapping("/trans/{id}")
+	public Transaction getTransactionById(@PathVariable String id) {
+		return transactionDao.getTransactionById(id);
+	}
+ 
+	@DeleteMapping("/trans/{id}")
+	public void deleteTranscationById(@PathVariable String id) {
+		transactionDao.deleteTranscationById(id);
+	}
+//	@GetMapping("/trans-info")
+//	public Long getMaxTransactionId() {
+//		return transactionDao.getMaxTransactionId();
+//	}
 	
 }
